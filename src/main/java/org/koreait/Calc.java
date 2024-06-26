f87208caf2fb0645551a8fdd8f98f652b5a9ff34
eunseo1030 <eunseo20001030@gmail.com>
2024-06-26 16:34:25 +0900

    10 + 20 + 30 - 10 + 60

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -1,36 +1,23 @@
 package org.koreait;
 
 public class Calc {
-    public static int run(String exp){
+    public static int run(String exp) {
 
-     boolean needToPlus = exp.contains("+");
-     boolean needToMinus = exp.contains("-");
+        exp = exp.replaceAll("- ", "+ -");
 
-     String[] bits = null;
-
-     if (needToPlus) {
-         bits = exp.split(" \\+ ");
-     } else if (needToMinus) {
-         bits = exp.split(" \\- ");
-
-     }
+        String[] bits = exp.split(" \\+ ");
 
 
-      int a = Integer.parseInt(bits[0]);
-      int b = Integer.parseInt(bits[1]);
-      int c = 0;
+         int sum = 0;
 
-      if(bits.length > 2){
-          c = Integer.parseInt(bits[2]);
-      }
 
-      if (needToPlus) {
-          return a + b + c;
-      } else if (needToMinus) {
-          return a - b - c;
-      }
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
 
-        throw new RuntimeException("해석불가: 올바른 계산이 아니야");
+
+            return sum;
+
+//        throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
+        }
     }
-
-}
