926fd09ad076659427fd4b3fc25379e05f660c56
eunseo1030 <eunseo20001030@gmail.com>
2024-06-26 15:05:35 +0900

    10 + 20 + 30

--- src/test/java/org/koreait/CalcTests.java
+++ src/test/java/org/koreait/CalcTests.java
@@ -55,4 +55,13 @@ public class CalcTests {
         assertThat(Calc.run("100 - 20")).isEqualTo(80);
     }
 
+    @Test
+    @DisplayName("10 + 20 + 30 == 60")
+    void t8() {
+        assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
+
+    }
+
+
+
 }
